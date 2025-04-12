f0c50b30c38f8a75ba49160564606a34183d6d87
Demm1le <Halikovadel26@gmail.com>
2025-04-12 15:23:52 +0300

    Update zadacha_3.c

--- zadacha_3.c
+++ zadacha_3.c
@@ -1,1 +1,8 @@
-ggg
+#include <stdio.h>
+
+int main() {
+    int colorful_balls[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int size = sizeof(colorful_balls) / sizeof(colorful_balls[0]);
+    printf("В коробке colorful_balls помещается %d разноцветных шариков.\n", size);
+    return 0;
+}
